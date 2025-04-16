007a9c43ef127e324187003f343937e54563a26a
paulo <kobeuko@gmail.com>
2025-04-16 04:25:01 -0500

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import ContactForm from "Contact";
-import NavBar from "Message";
-import { Body, Carousel, ListGroup } from "Underdog";
-import Footer from "Footer";
-import 'App.css'
+import ContactForm from "./Contact";
+import NavBar from "./Message";
+import { Body, Carousel, ListGroup } from "./Underdog";
+import Footer from "./Footer";
+import './App.css'
 function App(){
 
   return (
